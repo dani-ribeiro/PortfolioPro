31eb0ad5d97c01c86929179b8f5f0c02564ac318
Daniel <98674851+dani-ribeiro@users.noreply.github.com>
2023-06-06 23:32:47 -0400

    June 6 11:32 PM

--- Citadel-Project/app.py
+++ Citadel-Project/app.py
@@ -1,11 +1,14 @@
 import model
-
 from flask import Flask
 from flask import render_template, request, redirect, url_for
 import requests
 from pymongo import MongoClient
 
 app = Flask(__name__)
+user = {
+    'tickers': [],
+    'shares': []
+}
 
 # MONGO DB CONFIGURATION TODO: USERNAME/PASSWORD GENERATION
 username = "admin"
@@ -22,27 +25,42 @@ collection = db.stocks
 def show_home():
     return render_template("index.html")
 
+
 # USER INPUTS PAGE (INPUT STOCK INFORMATION)
 @app.route('/asset-inputs', methods = ['GET', 'POST'])
 def input_stocks():
+    global user
     if request.method == 'GET':
         return render_template('addPortfolio.html')
     else:
+        # TODO: change the way in which we obtain the data from the user (simplify request form)
         user = {
-            'tickers': request.form('assets'), # THIS WILL BE A LIST
-            'shares': request.form('shares') # THIS WILL BE A LIST OF INTEGERS EQUAL TO THE NUMBER OF ASSETS
+            'tickers': [
+                request.form['ticker1'],
+                request.form['ticker2'],
+                request.form['ticker3'],
+                request.form['ticker4'],
+                request.form['ticker5']
+            ], # THIS WILL BE A LIST
+            'shares': [
+                request.form['share1'], 
+                request.form['share2'], 
+                request.form['share3'], 
+                request.form['share4'], 
+                request.form['share5']
+                ] # THIS WILL BE A LIST OF INTEGERS EQUAL TO THE NUMBER OF ASSETS
         }
+        print("first test - " + str(user))
         return redirect('/results')
 
 # RESULTS PAGE OF CARDS
 @app.route('/results', methods = ['GET'])
 def results():
-    apikey = ""
-    # method to obtain tickers from portfolio
-    # results = call to model.cardCreation(user['tickers'], user['shares']) (results will be a dictionary)
-    return render_template('view.html', results = results)
-
-    
+    global user
+    print("second test -" + str(user))
+#     # method to obtain tickers from portfolio
+    stocks = model.cardCreation(user['tickers'], user['shares'])
+    return render_template('view.html', stocks = stocks) # TODO: add stocks
     
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 5000)
--- Citadel-Project/model.py
+++ Citadel-Project/model.py
@@ -1,11 +1,21 @@
 import requests
-apikey = 'D9ytM36GzrbaRllf8C8dN7BCgidRY3RfkBnjNJ2w'
-def port_calculation(ticker_list, shares_list):
-    zipped_lists = zip(ticker_list, shares_list)
-    for pair, i in enumerate(zipped_lists):
-        ticker, share = pair
-        requests.get(f"https://api.stockdata.org/v1/data/quote?symbols={ticker}")
 
+apikey = 'D9ytM36GzrbaRllf8C8dN7BCgidRY3RfkBnjNJ2w'
+def port_calculation(ticker_list, shares_list) -> list:
+    zipped_lists = zip(ticker_list, shares_list)
+    total = 0
+    price_list = []
+    for _, pair in enumerate(zipped_lists):
+        ticker, share = pair
+        comp_data = dict(requests.get(f"https://api.stockdata.org/v1/data/quote?symbols={ticker}&api_token={apikey}").json())
+        price = comp_data['data'][0]['price']
+        total += price*share
+        price_list.append(price)
+    
+    return [(ticker_list[i], round(shares_list[i]*price*100/total, 2)) for i, price in enumerate(price_list)]
+
+
+    
 
 def cardCreation(l1, l2) -> dict:
     # l1: list of tickers
@@ -14,8 +24,9 @@ def cardCreation(l1, l2) -> dict:
     apikey = 'D9ytM36GzrbaRllf8C8dN7BCgidRY3RfkBnjNJ2w'
     l3 = {}
     for ticker in l1:
-        val = requests.get("https://api.stockdata.org/v1/data/quote?symbols=" + ticker + "&api_token=" + apikey).json()
-        l3[ticker] = val['data'][0]    
+        val = dict(requests.get(f"https://api.stockdata.org/v1/data/quote?symbols={ticker}&api_token={apikey}").json())
+        print(val)
+        l3[ticker] = val['data'][0] # key: ticker; value: all data for the ticker from stockdata   
         
     return l3
  
